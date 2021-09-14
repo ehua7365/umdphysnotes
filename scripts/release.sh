b53975ad16d827c77d4a17552b7049b370b0ae57
ehua7365 <ehua7365@users.noreply.github.com>
2021-09-14 13:04:48 -0400

    [Change] fix bug in release script
    Wrong file extension fixed.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 date=$(date +'%Y-%m-%d')
 for course in phys610 phys612 phys733; do
-    tex_pdf="${course}/main.tex"
+    tex_pdf="${course}/main.pdf"
     release_pdf="release/${date}_${course}_notes.pdf"
     if [ -f $input_pdf ]; then
         cp $tex_pdf $release_pdf
